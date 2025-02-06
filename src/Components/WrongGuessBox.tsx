5e848918a157d7ade2c8679a6dc83012ffccb81f
Bravin Atonya <bravinatonya.brani@gmail.com>
2025-02-06 10:49:11 +0300

    Wrong guess notifier
    Implementation of a notifier that will help with notifying the users
    when they make a wrong guess

---
+++ src/Components/WrongGuessBox.tsx
@@ -0,0 +1,9 @@
+const WrongGuessBox = () => {
+    return (
+        <div className="wrongGuessBox roboto-medium" data-testid="gameStatus">
+            <div className="mysteryText">Wrong Guess</div>
+        </div>
+    );
+};
+
+export default WrongGuessBox;

7dbc724a22de1c14ce5ef0ef6ae329f01d1ff904
Bravin Atonya <bravinatonya.brani@gmail.com>
2025-02-06 10:45:47 +0300

    Correct guess notifier
    Implementation of a box that will notify the user of correct guesses.

---
+++ src/Components/CorrectGuessBox.tsx
@@ -0,0 +1,9 @@
+const CorrectGuessBox = () => {
+    return (
+        <div className="correctGuessBox roboto-medium" data-testid="gameStatus">
+            <div className="mysteryText">Correct Guess</div>
+        </div>
+    );
+};
+
+export default CorrectGuessBox;
